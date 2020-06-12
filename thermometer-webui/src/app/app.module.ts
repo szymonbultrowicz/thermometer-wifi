b2bd9d7907323b21256165fe432092e93d02a486
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2020-06-12 20:46:54 +0200

    feat(webui)) Empty page

--- thermometer-webui/src/app/app.module.ts
+++ thermometer-webui/src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Error401Component } from './error401/error401.component';
+
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: '/dashboard',
+}, {
+  path: 'dashboard',
+  component: DashboardComponent,
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error401Component,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
