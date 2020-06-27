30b7f5bbd706863eb7848bd0723efbcce3f9bacd
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2020-06-27 02:44:47 +0200

    feat(webui) better charts

--- thermometer-webui/src/app/dashboard/dashboard.component.ts
+++ thermometer-webui/src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { map } from 'rxjs/operators';
 import { Reading } from '../data';
 import { DataService } from './../data/data.service';
 import { Series } from '@swimlane/ngx-charts';
+import { maxBy, minBy } from 'lodash-es';
+import { NEVER } from 'rxjs';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -11,8 +13,23 @@ import { Series } from '@swimlane/ngx-charts';
 })
 export class DashboardComponent implements OnInit {
 
-  lastValue$: Observable<Reading>;
-  series$: Observable<Series[]>
+  lastValue$: Observable<Reading> = NEVER;
+  temperature$: Observable<Series[]> = NEVER;
+  humidity$: Observable<Series[]> = NEVER;
+  battery$: Observable<Series[]> = NEVER;
+
+  temperatureBorders$: Observable<{min: number, max: number}> = NEVER;
+
+  readonly colors = [{
+    name: 'temperature',
+    value: '#3cff00',
+  }, {
+    name: 'humidity',
+    value: '#00fff9',
+  }, {
+    name: 'battery',
+    value: '#fff400',
+  }];
 
   constructor(
     private readonly dataService: DataService,
@@ -22,7 +39,30 @@ export class DashboardComponent implements OnInit {
     this.lastValue$ = this.dataService.lastValue$.pipe(
       map(data => data.result),
     );
-    this.series$ = this.dataService.series$;
+    this.temperature$ = this.dataService.series$.pipe(
+      map(series => [series.temperature]),
+    );
+    this.humidity$ = this.dataService.series$.pipe(
+      map(series => [series.humidity]),
+    );
+    this.battery$ = this.dataService.series$.pipe(
+      map(series => [series.battery]),
+    );
+
+    this.temperatureBorders$ = this.temperature$.pipe(
+      map(([series]) => ({
+        max: this.maxValue(series),
+        min: this.minValue(series),
+      })),
+    );
+  }
+
+  private maxValue(series: Series): number {
+    return Math.ceil(maxBy(series.series, 'value')?.value as (number | undefined) ?? 0);
+  }
+
+  private minValue(series: Series): number {
+    return Math.floor(minBy(series.series, 'value')?.value as (number | undefined) ?? 0);
   }
 
 }
--- thermometer-webui/src/app/data/data.service.ts
+++ thermometer-webui/src/app/data/data.service.ts
@@ -1,44 +1,37 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, switchMap, map } from 'rxjs/operators';
+import { filter, switchMap, map, shareReplay } from 'rxjs/operators';
 import { TokenService } from './../auth/token.service';
 import { HistoricalData, LastValue, Reading } from './models';
 import { Series } from '@swimlane/ngx-charts';
 
 const ENDPOINT_BASE = environment.API_URL;
-const DAY_MILLIS = 1000 * 60 * 60 * 24;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  readonly historicalData$ = this.requestWithToken<HistoricalData>(`${ENDPOINT_BASE}/history`);
+  readonly lastValue$ = this.requestWithToken<LastValue>(`${ENDPOINT_BASE}/last`);
+
   constructor(
     private readonly httpClient: HttpClient,
     private readonly tokenService: TokenService,
   ) { }
 
-  get lastValue$() {
-    return this.requestWithToken<LastValue>(`${ENDPOINT_BASE}/last`);
-  }
-
-  get historicalData$() {
-    return this.requestWithToken<HistoricalData>(`${ENDPOINT_BASE}/history`);
-  }
-
   get series$() {
     return this.historicalData$.pipe(
-      map(data => [
-        this.retrieveSeries(data, 'temperature'),
-        this.retrieveSeries(data, 'humidity'),
-        this.retrieveSeries(data, 'battery'),
-      ]),
+      map(data => ({
+        temperature: this.retrieveSeries(data, 'temperature'),
+        humidity: this.retrieveSeries(data, 'humidity'),
+        battery: this.retrieveSeries(data, 'battery'),
+      })),
     );
   }
 
   private retrieveSeries(data: HistoricalData, series: keyof Reading): Series {
-    console.log(data.result)
     const values = data.result.map(entry => ({
       name: new Date(entry.timestamp),
       value: entry[series],
@@ -57,6 +50,7 @@ export class DataService {
           Authorization: token,
         },
       })),
+      shareReplay(),
     );
   }
 }
