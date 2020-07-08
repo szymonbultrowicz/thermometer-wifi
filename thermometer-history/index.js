922de952ddf94fd0b292b3941cf2e7533fdfdab2
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2020-07-08 23:25:58 +0200

    feat(lambda-history) Proper support for adaptive aggregation

--- thermometer-history/index.js
+++ thermometer-history/index.js
@@ -4,6 +4,21 @@ const { InfluxDB } = require('@influxdata/influxdb-client')
 const DAY_MILIS = 24 * 60 * 60 * 1000;
 const { INFLUX_URL, INFLUX_ORG, INFLUX_BUCKET, INFLUX_TOKEN } = process.env;
 
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const WEEK = 7 * DAY;
+const MONTH = 30 * DAY;
+
+const timeMultipliers = {
+    's': 1,
+    'm': MINUTE,
+    'h': HOUR,
+    'd': DAY,
+    'w': WEEK,
+    'mo': MONTH,
+};
+
 const client = new InfluxDB({url: 'https://eu-central-1-1.aws.cloud2.influxdata.com', token: INFLUX_TOKEN});
 const queryApi = client.getQueryApi(INFLUX_ORG);
 
@@ -11,10 +26,9 @@ exports.handler = async (event) => {
     try {
         switch (event.httpMethod) {
             case 'GET':
-                console.log(event);
-                const from = (event.pathParameters || {}).from || '-1d';
-                validateDuration(from);
-                const result = await fetchData(from);
+                const timeframe = (event.queryStringParameters || {}).timeframe || '1d';
+                validateDuration(timeframe);
+                const result = await fetchData(timeframe.startsWith('-') ? timeframe : `-${timeframe}`);
                 return buildResponse(200, {
                     result,
                 });
@@ -29,12 +43,45 @@ exports.handler = async (event) => {
     }
 };
 
-const fetchData = async (from) => {
+const aggregationWindow = (timeframe) => {
+    const [,amountStr, unit] = timeframe.match(/-?(\d+)([a-zA-Z]{1,2})$/);
+    const multiplier = timeMultipliers[unit];
+    if (!multiplier) {
+        throw Error(`Invalid unit: ${unit}`);
+    }
+    const amount = parseInt(amountStr, 10);
+    const windowSec = amount * multiplier;
+    if (windowSec <= 15 * MINUTE) {
+        return '1s';
+    }
+    if (windowSec <= 2 * HOUR) {
+        return '1m';
+    }
+    if (windowSec <= DAY) {
+        return '15m';
+    }
+    if (windowSec <= DAY) {
+        return '15m';
+    }
+    if (windowSec <= WEEK) {
+        return '1h';
+    }
+    if (windowSec <= MONTH) {
+        return '3h';
+    }
+    return '1h';
+};
+
+const fetchData = async (from, ) => {
     const query = `from(bucket: "thermometer")
 |> range(start: ${from}, stop: now())
 |> filter(fn: (r) => r["_measurement"] == "readings")
+|> aggregateWindow(every: ${aggregationWindow(from)}, fn: mean)
 `;
+    const startTime = Date.now();
     const rows = await fetchQuery(query);
+    const endTime = Date.now();
+    console.log(`InfluxDB query took ${endTime - startTime}ms`);
     return rows;
 }
 
@@ -78,7 +125,7 @@ const buildResponse = (statusCode, body, headers = {}) => ({
 })
 
 function validateDuration(value) {
-    if (!value.match(/^-?\d+[a-z]$/)) {
+    if (!value.match(/^-?\d+(s|m|h|d|w|mo)$/)) {
         throw Error('Invalid duration: ' + value);
     }
 }
