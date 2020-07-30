cf4e2e5f47f6b99cab0bc62a49b9e829cb037faf
Szymon Bultrowicz <szymonbultrowicz@users.noreply.github.com>
2020-07-30 23:55:57 +0200

    chore(webui) Deployment script

---
+++ thermometer-webui/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const rsync = require('gulp-rsync');
+
+const DIST_PATH = 'dist/thermometer-webui';
+
+require('dotenv').config();
+
+const deploymentHost = process.env.DEPLOYMENT_HOST;
+const deploymentPath = process.env.DEPLOYMENT_PATH;
+
+gulp.task('deploy', () => 
+    gulp.src(`${DIST_PATH}/**`)
+        .pipe(rsync({
+            hostname: deploymentHost,
+            root: `${DIST_PATH}/`,
+            destination: deploymentPath,
+            archive: true,
+            silent: false,
+            compress: true,
+            chmod: "ugo=rwX",
+            clean: true,
+        }))
+);
